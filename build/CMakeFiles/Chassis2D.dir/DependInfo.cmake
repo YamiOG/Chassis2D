0529ef7ea22e0c1db1b2d26c14c680ce172f1ace
YamiOG <noahy1214@gmail.com>
2020-07-29 08:48:33 -0400

    Updated Travis CI

---
+++ build/CMakeFiles/Chassis2D.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/App.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/App.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/Button.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/Button.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/Contact.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/Contact.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/Entity.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/Entity.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/Font.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/Font.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/Network.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/Network.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/Object.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/Object.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/Particle.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/Particle.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/ParticleSystem.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/ParticleSystem.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/Sound.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/Sound.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/Text.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/Text.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/Texture.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/Texture.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/chassis/Timer.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/chassis/Timer.cpp.o"
+  "/media/noah/AEE22B36E22B0267/Code/GameFramework/src/main.cpp" "/media/noah/AEE22B36E22B0267/Code/GameFramework/build/CMakeFiles/Chassis2D.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../vcpkg/installed/x64-linux/include"
+  "../vcpkg/installed/x64-linux/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
