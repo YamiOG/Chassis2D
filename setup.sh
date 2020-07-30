6f3d49a4779d5f508804ebb5abc2c98e4d3ab8e3
YamiOG <noahy1214@gmail.com>
2020-07-29 20:53:40 -0400

    Test

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://github.com/microsoft/vcpkg.git
+./vcpkg/bootstrap-vcpkg.sh
+
+./vcpkg/vcpkg install sdl2
+./vcpkg/vcpkg install sdl2-image
+./vcpkg/vcpkg install sdl2-mixer
+./vcpkg/vcpkg install sdl2-ttf
+./vcpkg/vcpkg install sdl2-net
+./vcpkg/vcpkg install box2d
+
