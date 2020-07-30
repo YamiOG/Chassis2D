aafac2ce8417f9dc522cbea2ef4d664ada478fb3
YamiOG <noahy1214@gmail.com>
2020-07-29 23:07:05 -0400

    Polished Up Some of the Shell Script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-git clone https://github.com/microsoft/vcpkg.git
+if [ -d "vcpkg" ]; then
+   if [ -z "$(ls -A vcpkg/)" ]; then
+	   rm -rf vcpkg
+	   git clone https://github.com/microsoft/vcpkg.git
+   fi
+fi
 
 ./vcpkg/bootstrap-vcpkg.sh
 ./vcpkg/vcpkg install sdl2 sdl2-image sdl2-mixer sdl2-ttf sdl2-net box2d
