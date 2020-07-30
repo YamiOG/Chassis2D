49e4d29fb1a195acd60e03ce0f8d0f22030d68e2
YamiOG <noahy1214@gmail.com>
2020-07-30 12:19:04 -0400

    Added Setup File For Windows

---
+++ setup.bat
@@ -0,0 +1,28 @@
+@echo off
+
+IF EXIST vcpkg (
+	set _TMP=
+	for /f "delims=" %%a in ('dir /b "vcpkg"') do set _TMP="%%a"
+	IF NOT {%_TMP%}=={} (
+		rmdir vcpkg
+		curl -LJO https://github.com/microsoft/vcpkg/archive/master.zip
+		tar -xf vcpkg-master.zip
+		del vcpkg-master.zip
+		rename vcpkg-master vcpkg
+	)
+) ELSE (
+	curl -LJO https://github.com/microsoft/vcpkg/archive/master.zip
+	tar -xf vcpkg-master.zip
+	del vcpkg-master.zip
+	rename vcpkg-master vcpkg
+)
+
+.\vcpkg\bootstrap-vcpkg.bat
+
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+	.\vcpkg\vcpkg.exe install sdl2 sdl2-image sdl2-mixer sdl2-ttf sdl2-net box2d --triplet x64-windows
+) ELSE (
+	.\vcpkg\vcpkg.exe install sdl2 sdl2-image sdl2-mixer sdl2-ttf sdl2-net box2d --triplet x86-windows
+)
+
+PAUSE
